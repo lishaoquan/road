6df8fdd92783f77d8e91789c28fbb41ad90c7ac7
king_aust <136499160@qq.com>
2014-08-16 23:29:33 +0800

    把JFinal的包放入到系统中

---
+++ contact/src/com/contact/config/ContactRoutes.java
@@ -0,0 +1,10 @@
+package com.contact.config;
+
+import com.jfinal.config.Routes;
+
+public class ContactRoutes extends Routes {
+	public void config() {
+		// add("/hello", HelloController.class);
+
+	}
+}
