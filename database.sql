5d84671af008a26ce672ddfd7e612a2eac8d29ea
lishaoquan <526895661@qq.com>
2014-08-17 20:34:25 +0800

    用户表sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 --合同系统建表语句
 --version1.0
 
-
+use contact;
 --菜单表
 CREATE TABLE `contact`.`menu` (
   `menuid` VARCHAR(32) NOT NULL,
@@ -10,4 +10,15 @@ CREATE TABLE `contact`.`menu` (
   `parentId` VARCHAR(32) NULL,
   `url` VARCHAR(255) NULL,
   PRIMARY KEY (`menuid`))
+ENGINE = InnoDB;
+
+
+--用户表
+CREATE TABLE `contact`.`user` (
+  `id` VARCHAR(32) NOT NULL,
+  `userName` VARCHAR(45) NULL,
+  `email` VARCHAR(255) NULL,
+  `userId` VARCHAR(32) NULL,
+  `password` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
