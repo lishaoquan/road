e5c5cac341ae26b1bc61cc002894fabd7e772588
lishaoquan <526895661@qq.com>
2014-08-18 21:10:14 +0800

    添加用户数据库操作

---
+++ contact/src/com/contact/constant/Constant.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.contact.constant;
+
+/**
+ * 常量类
+ * @author Administrator
+ *
+ */
+public class Constant {
+
+	public class DATABASE_NAME {
+		public static final String CONTACT = "contact";
+
+	}
+	
+	public class TABLE_NAME {
+
+		public static final String USER = "user";
+
+		public static final String MENU = "menu";
+
+	}
+}
---
+++ contact/src/com/contact/user/db/UserDao.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.contact.user.db;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.contact.constant.Constant;
+import com.contact.user.model.User;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+
+/**
+ * 提供用户操作数据库的工具
+ * @author Administrator
+ *
+ */
+public class UserDao {
+
+	/**
+	 * 根据主键获取用户信息
+	 * @param id
+	 * @return user
+	 */
+	public static User findUserById(String id){
+		Record record = Db.findById(Constant.TABLE_NAME.USER,"id",id,"id,userName,email,userId,password");
+		User user = null;
+		if (null != record && StringUtils.isNotBlank(record.getStr("id"))){
+			user = new User();
+			user.setId(record.getStr("id"));
+			user.setEmail(record.getStr("email"));
+			user.setPassword(record.getStr("password"));
+			user.setUserId(record.getStr("userId"));
+			user.setUserName(record.getStr("userName"));
+		}
+		return user;
+	}
+	
+	/**
+	 * 根据用户登录名获取用户信息
+	 * @param id
+	 * @return user
+	 */
+	public static User findUserByUserId(String userId){
+		Record record = Db.findFirst("select * from " + Constant.TABLE_NAME.USER + " where userId ='" + userId + "'");
+		User user = null;
+		if (null != record && StringUtils.isNotBlank(record.getStr("id"))){
+			user = new User();
+			user.setId(record.getStr("id"));
+			user.setEmail(record.getStr("email"));
+			user.setPassword(record.getStr("password"));
+			user.setUserId(record.getStr("userId"));
+			user.setUserName(record.getStr("userName"));
+		}
+		return user;
+	}
+}
