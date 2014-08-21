0bc9082c6e0e8c9e03c590f6d6bc88b736cccf3d
lishaoquan <526895661@qq.com>
2014-08-21 12:38:54 +0800

    用户表和菜单表的初始化语句

--- database.sql
+++ database.sql
@@ -48,3 +48,29 @@ CREATE TABLE `contact`.`role` (
   `desc` VARCHAR(255) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+
+
+
+
+----初始化数据库表数据
+/*
+-- Query: SELECT * FROM contact.user
+LIMIT 0, 1000
+
+-- Date: 2014-08-21 12:34
+*/
+INSERT INTO `contact`.`user` (`id`,`userName`,`email`,`userId`,`password`) VALUES ('1','系统管理员','admin@126.com','admin','123456');
+
+/*
+-- Query: SELECT * FROM contact.menu
+LIMIT 0, 1000
+
+-- Date: 2014-08-21 12:35
+*/
+INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('1','系统菜单',-1,'-1','');
+INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('2','系统管理',1,'1','');
+INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('3','菜单管理',2,'2','/pages/menu/menu.jsp');
+INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('555540fa72d7408e9b4fcf95e3b999af','用户管理',4,'2','/pages/user/user.jsp');
+INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('a03f7a0c564d4259bd35bf533bd7b1e2','角色管理',5,'2','/pages/role/role.jsp');
+INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('fe4d065e248b4a59aae41750ebf3ff8a','权限管理',3,'2','/pages/auth/auth.jsp');
