d89b025e54113a8baed8ad27f8d7971891392542
lishaoquan <526895661@qq.com>
2014-08-28 22:01:49 +0800

    sql语句

--- database.sql
+++ database.sql
@@ -154,21 +154,22 @@ LIMIT 0, 1000
 -- Date: 2014-08-24 19:49
 */
 DELETE FROM `contact`.`menu`;
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('1','系统菜单',-1,'-1','');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('2','系统管理',1,'1','');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('229cbede3d0c407e9873bec290f94e4c','采购管理',5,'1','');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('24ff661701f94152b73c664f4c2840ff','新订单生产安排',1,'e4344e2fd4ee464da1988a3a28020922','/pages/production/production.jsp');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('3','菜单管理',2,'2','/pages/menu/menu.jsp');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('45726280f95e4e449a0d4c905590a480','产品管理',2,'1','');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('555540fa72d7408e9b4fcf95e3b999af','用户管理',4,'2','/pages/user/user.jsp');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('75990c366b874b219421d2a27b7032a3','从编码生成合同',2,'d7cb729f583940ac900d574aab331b03','/pages/contract/fromcode.jsp');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('8856968cfe674bcfb3d46da561da56e0','产品分类管理',1,'45726280f95e4e449a0d4c905590a480','/pages/productcategory/productcategory.jsp');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('a03f7a0c564d4259bd35bf533bd7b1e2','角色管理',5,'2','/pages/role/role.jsp');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('bd5b09b6ee1d48e2ad6522819aa09699','采购管理',1,'229cbede3d0c407e9873bec290f94e4c','/pages/purchase/purchase.jsp');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('d7cb729f583940ac900d574aab331b03','销售管理',3,'1','');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('de7cf5961cfb4fe49163e727a13ce45c','从列表生成合同',1,'d7cb729f583940ac900d574aab331b03','/pages/sale/listcontract/productlist.jsp');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('e4344e2fd4ee464da1988a3a28020922','生产管理',4,'1','');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('e8e4c1c52554474e9770915e8d35ec65','合同管理',3,'d7cb729f583940ac900d574aab331b03','/pages/contract/contract.jsp');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('ee200f60e1f340f8aad659515f0e8507','产品管理',2,'45726280f95e4e449a0d4c905590a480','/pages/product/product.jsp');
-INSERT INTO `contact`.`menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('fe4d065e248b4a59aae41750ebf3ff8a','权限管理',3,'2','/pages/auth/auth.jsp');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('1','系统菜单',-1,'-1','');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('2','系统管理',1,'1','');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('229cbede3d0c407e9873bec290f94e4c','采购管理',5,'1','');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('24ff661701f94152b73c664f4c2840ff','新订单生产安排',1,'e4344e2fd4ee464da1988a3a28020922','/pages/production/production.jsp');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('3','菜单管理',2,'2','/pages/menu/menu.jsp');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('45726280f95e4e449a0d4c905590a480','产品管理',2,'1','');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('555540fa72d7408e9b4fcf95e3b999af','用户管理',4,'2','/pages/user/user.jsp');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('75990c366b874b219421d2a27b7032a3','从编码生成合同',2,'d7cb729f583940ac900d574aab331b03','/pages/contract/fromcode.jsp');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('8856968cfe674bcfb3d46da561da56e0','产品分类管理',1,'45726280f95e4e449a0d4c905590a480','/pages/productcategory/productcategory.jsp');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('a03f7a0c564d4259bd35bf533bd7b1e2','角色管理',5,'2','/pages/role/role.jsp');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('bd5b09b6ee1d48e2ad6522819aa09699','采购管理',1,'229cbede3d0c407e9873bec290f94e4c','/pages/purchase/purchase.jsp');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('d7cb729f583940ac900d574aab331b03','销售管理',3,'1','');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('de7cf5961cfb4fe49163e727a13ce45c','从列表生成合同',1,'d7cb729f583940ac900d574aab331b03','/pages/sale/listcontract/productlist.jsp');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('e4344e2fd4ee464da1988a3a28020922','生产管理',4,'1','');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('e8e4c1c52554474e9770915e8d35ec65','合同管理',3,'d7cb729f583940ac900d574aab331b03','/pages/contract/contractManager.jsp');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('ee200f60e1f340f8aad659515f0e8507','产品管理',2,'45726280f95e4e449a0d4c905590a480','/pages/product/product.jsp');
+INSERT INTO `menu` (`menuid`,`menuName`,`sort`,`parentId`,`url`) VALUES ('fe4d065e248b4a59aae41750ebf3ff8a','权限管理',3,'2','/pages/auth/auth.jsp');
+
 
